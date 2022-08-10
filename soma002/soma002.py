b11e687dda45ceab6ce15484a5425337e7bf1e96
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2022-08-09 21:34:08 -0300

    Add files via upload

---
+++ soma002/soma002.py
@@ -0,0 +1,32 @@
+"""
+Programa soma
+Descrição:
+Este programa soma dois números dados pelo programador e imprime
+na tela o resultado com uma frase explicativa.
+Autor: Nelson Seixas dos Santos
+Versão: 0.0.2
+Data: 09/08/2022
+"""
+
+# Entrada de dados
+
+# Esta instrução diz para o computador guardar o número 2 em um
+# endereço de memória e "apelidar" este endereço de numero_1
+numero_1 = 2
+
+
+# Esta instrução diz para o computador guardar o número 3 em um
+# endereço de memória e "apelidar" este endereço de numero_2
+numero_2 = 3
+
+
+# Processamento de dados
+
+soma = numero_1 + numero_2
+
+# Saida de dados
+
+# f-string
+print(f"O resultado da soma do {numero_1} com o {numero_2} é igual a {soma}.")
+
+
---
+++ soma004/soma004.py
@@ -0,0 +1,33 @@
+"""
+Programa soma
+Descrição:
+Este programa soma dois números dados pelo usuário e imprime
+na tela o resultado com uma frase explicativa.
+Autor: Nelson Seixas dos Santos
+Versão: 0.0.4 
+Data: 09/08/2022
+"""
+
+
+
+# Entrada de dados
+
+# input é uma função que permite fazer a leitura do teclado do usuário
+# durante a execução do programa
+numero_1 = float(input("Entre a primeira parcela."))
+
+
+
+numero_2 = float(input("Entre a segunda parcela."))
+
+
+# Processamento de dados
+
+soma = numero_1 + numero_2
+
+# Saida de dados
+
+# f-string
+print(f"O resultado da soma do {numero_1} com o {numero_2} é igual a {soma}.")
+
+
