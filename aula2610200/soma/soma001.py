ce7d1cf5e74c109016d06b160254165541fd0b2b
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2022-10-26 21:22:33 -0300

    Update soma001.py

--- aula2610200/soma/soma001.py
+++ aula2610200/soma/soma001.py
@@ -1,1 +1,28 @@
 
+"""
+Programa soma
+Requisito: Este programa pega dois números digitados pelo
+programador e calcula a sua soma.
+Autor: Nelson S. dos Santos
+Data: 26/10/2022
+Versão: 0.0.1
+"""
+
+# Entrada
+
+numero_1 = 7
+
+numero_2 = 3.3
+
+
+# Processamento
+
+soma = numero_1 + numero_2
+
+# Saída
+
+print(soma)
+
+
+
+
