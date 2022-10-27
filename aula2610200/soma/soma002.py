3766d52144e900f705949e459acf9a225f7c2b5b
profnelsonseixas <74307897+profnelsonseixas@users.noreply.github.com>
2022-10-26 21:23:23 -0300

    Add files via upload

---
+++ aula2610200/soma/soma002.py
@@ -0,0 +1,28 @@
+"""
+Programa soma
+Requisito: Este programa pega dois números digitados pelo
+programador, calcula a sua soma e coloca o resultado na tela,
+com uma frase que explique o que é o resultado obtido.
+Autor: Nelson S. dos Santos
+Data: 26/10/2022
+Versão: 0.0.2
+"""
+
+# Entrada
+
+numero_1 = 7
+
+numero_2 = 3.3
+
+
+# Processamento
+
+soma = numero_1 + numero_2
+
+# Saída
+
+print(f"A soma dos números {numero_1} e {numero_2} é igual a {soma} .")
+
+
+
+
---
+++ aula2610200/soma/soma003.py
@@ -0,0 +1,29 @@
+"""
+Programa soma
+Requisito: Este programa pega dois números digitados pelo
+usuário, calcula a sua soma e coloca o resultado na tela,
+com uma frase que explique o que é o resultado obtido.
+Autor: Nelson S. dos Santos
+Data: 26/10/2022
+Versão: 0.0.3
+"""
+
+# Entrada
+
+numero_1 = float(input("Digite a primeira parcela: "))
+
+numero_2 = float(input("Digite a segunda parcela: "))
+
+
+
+# Processamento
+
+soma = numero_1 + numero_2
+
+# Saída
+
+print(f"A soma dos números {numero_1} e {numero_2} é igual a {soma} .")
+
+
+
+
